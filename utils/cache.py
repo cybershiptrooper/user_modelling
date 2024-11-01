1f5c3fae807633fabfcf9bf507366fb9f38c33de
cybershiptrooper <cybershiptrooper@gmail.com>
2024-11-01 01:38:31 +0000

    check base model performance

--- utils/cache.py
+++ utils/cache.py
@@ -2,6 +2,7 @@ import transformer_lens as tl
 import torch
 from typing import Callable, Literal
 from tqdm import tqdm
+from typing import Callable
 
 
 def get_cache_fwd_and_bwd(
@@ -115,7 +116,7 @@ def batched_fwd_cache(
     device: torch.device = torch.device("cpu"),
     hook_point_substring: str = "resid_post",
     padding_side: Literal["left", "right"] = "right",
-    pos_slice: slice | None = None,
+    pos_slice: slice | Callable | None = None,
 ):
     cache = {}
     prompt_tokenized = model.to_tokens(
@@ -129,7 +130,12 @@ def batched_fwd_cache(
         if pos_slice is None:
             to_cache = act.detach().to(device)
         else:
-            to_cache = (act[pos_slice]).detach().to(device)
+            if isinstance(pos_slice, slice):
+                to_cache = (act[pos_slice]).detach().to(device)
+            elif callable(pos_slice):
+                to_cache = pos_slice(act).detach().to(device)
+            else:
+                raise ValueError(f"Invalid pos_slice object: {pos_slice}")
         if hook.name in cache:
             cache[hook.name] = torch.cat([cache[hook.name], to_cache], dim=0)
         else:
@@ -142,6 +148,52 @@ def batched_fwd_cache(
         with model.hooks(fwd_hooks=caching_hooks):
             model(prompt_tokenized[i : i + batch_size], return_type="logits")
         torch.cuda.empty_cache()
+    return tl.ActivationCache(cache, model)
+
+
+
+def get_cache_at_last_tok(
+    model: tl.HookedTransformer,
+    prompts: list[str],
+    batch_size: int = 8,
+    device: torch.device = torch.device("cpu"),
+    hook_point_substring: str = "resid_post",
+    return_output: bool = False,
+):
+    '''
+    Cache the activations at the last token of each prompt in the batch.
+    '''
+    cache = {}
+    prompt_tokenized = model.to_tokens(
+        prompts, padding_side="right", prepend_bos=True
+    )
+    last_tok_ids = [
+        model.to_tokens(prompts[i], padding_side="right", prepend_bos=True).shape[1] - 1
+        for i in range(len(prompts))
+    ]
+    hook_points = [
+        hook.name for hook in model.hook_points() if hook_point_substring in hook.name
+    ]
+
+    def make_hook_fn(pos_slice: list[int]):
+        def hook_fn(act, hook):
+            to_cache = (act[torch.arange(act.shape[0]), pos_slice, :]).detach().to(device)
+            if hook.name in cache:
+                cache[hook.name] = torch.cat([cache[hook.name], to_cache], dim=0)
+            else:
+                cache[hook.name] = to_cache
+        return hook_fn
+    outs = []
+    for i in tqdm(range(0, len(prompts), batch_size)):
+        torch.cuda.empty_cache()
+        hook_fn = make_hook_fn(last_tok_ids[i : i + batch_size])
+        caching_hooks = [(hook_point, hook_fn) for hook_point in hook_points]
+        with model.hooks(fwd_hooks=caching_hooks):
+            out = model(prompt_tokenized[i : i + batch_size], return_type="logits").to(device)
+            outs.append(out[torch.arange(out.shape[0]), last_tok_ids[i : i + batch_size], :])
+        torch.cuda.empty_cache()
+    if return_output:
+        return torch.cat(outs, dim=0), tl.ActivationCache(cache, model)
     return tl.ActivationCache(cache, model)
 
 
